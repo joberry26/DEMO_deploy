554fa34ea8e373afb4d7557fd123ff5c127e8eb6
Joseph Berry <josephberry910@gmail.com>
2021-10-12 16:01:25 -0600

    deploy 2.0

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use('/style', express.static('./public/style.css'))
+app.use('/style', express.static('./public/styles.css'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
