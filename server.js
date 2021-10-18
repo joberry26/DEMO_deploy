dad155a803f47336eb03b6e7a19b1484cd17c059
Joseph Berry <josephberry910@gmail.com>
2021-10-18 12:19:26 -0600

    trying to load images

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-app.get('/', (req, res) => {
+app.get('/main.js', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/main.js'))
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/img'))
-});
 
 app.get('/products.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/products.html'))
@@ -23,6 +20,26 @@ app.get('/products.html', (req, res) => {
 
 app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/img/bark2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/img/bark2'))
+})
+
+app.get('/img/flower2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/img/flower2'))
+})
+
+app.get('/img/mountain2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/img/mountain2'))
+})
+
+app.get('/img/puddle2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/img/puddle2'))
+})
+
+app.get('/img/sunset2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/img/sunset2'))
 })
 
 
