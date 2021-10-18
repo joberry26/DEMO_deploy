dc387758e9a1f26783e7b090dcf5756ae275db24
Joseph Berry <josephberry910@gmail.com>
2021-10-18 10:04:11 -0600

    sub html page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-app.get('/', (req, res) => {
+app.get('/products.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/products.html'))
 })
 
